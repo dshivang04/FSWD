c3f2129d6915f926b0413dc690e2fd59f909dfc2
dshivang04 <dshivang0402@gmail.com>
2023-08-11 11:12:14 +0530

    P7

--- P7/Task1/task1/src/productListComponent.jsx
+++ P7/Task1/task1/src/productListComponent.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-
+import './Style.css';
 function Mylist() {
 
-    const products=[{
+    const products = [{
         id: "01",
         name: "Coffee",
         desc: "A COFFEE A DAY KEEPS THE DOCTOR AWAY.",
         price: 20,
-    },{
+    }, {
         id: "02",
         name: "Boost",
         desc: "Boost is the secret of our energy.",
         price: 20,
-    },{
+    }, {
         id: "03",
         name: "Tea",
         desc: "Kuch na ho toh ye chalega.",
@@ -20,15 +20,18 @@ function Mylist() {
     }]
 
     return (
-        <>
-            <div class="card">{
-                    products.map((item) => {
-
-                    })
-                }
-            </div>
-        </>
-
+        <>{
+            products.map((item) => {
+                return (
+                    <div class="card">
+                        <h1>{item.name}</h1>
+                        <p class="price">{item.price}</p>
+                        <p>{item.desc}</p>
+                        <p><button>Add to Cart</button></p>
+                    </div>
+                )
+            })
+        }</>
     )
 }
 
