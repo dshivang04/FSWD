e148b6e8ecee31f497ac3299c10410bf18e5654c
dshivang04 <dshivang0402@gmail.com>
2023-08-18 10:33:39 +0530

    P7

--- P7/Task1/task1/src/productListComponent.jsx
+++ P7/Task1/task1/src/productListComponent.jsx
@@ -4,16 +4,19 @@ function Mylist() {
 
     const products = [{
         id: "01",
+        img: <img src={require('./coffee.jpg') } />,
         name: "Coffee",
         desc: "A COFFEE A DAY KEEPS THE DOCTOR AWAY.",
         price: 20,
     }, {
         id: "02",
+        img: <img src={require('./boost.jpg') } />,
         name: "Boost",
         desc: "Boost is the secret of our energy.",
         price: 20,
     }, {
         id: "03",
+        img: <img src={require('./tea.jpg') } />,
         name: "Tea",
         desc: "Kuch na ho toh ye chalega.",
         price: 20,
@@ -25,7 +28,8 @@ function Mylist() {
                 return (
                     <div class="card">
                         <h1>{item.name}</h1>
-                        <p class="price">{item.price}</p>
+                        <image>{item.img}</image>
+                        <p class="price">Rs. {item.price}</p>
                         <p>{item.desc}</p>
                         <p><button>Add to Cart</button></p>
                     </div>
